304188681dac90b9e58704ff368078950b3737ac
Abinash Mohapatra <abinash@expensebit.com>
2020-12-10 14:47:19 +0530

    design the post section

--- src/components/Feed/Feed.js
+++ src/components/Feed/Feed.js
@@ -2,12 +2,20 @@ import React from 'react';
 
 import StoryReel from './StoryReel/StoryReel';
 import MessageSender from './MessageSender/MessageSender';
+import Post from './Post/Post';
 
 const Feed = () => {
     return (
         <div className="feed">
             <StoryReel />
             <MessageSender />
+            <Post
+                profilePic="https://i.pinimg.com/originals/51/f6/fb/51f6fb256629fc755b8870c801092942.png"
+                message="Yo This is the message"
+                timestamp='1601493943737'
+                imgName="iamgeName"
+                username="Abinash Mohapatra"
+            />
         </div>
     )
 }
---
+++ src/components/Feed/Post/Post.js
@@ -0,0 +1,44 @@
+import React from 'react';
+
+import { Avatar } from '@material-ui/core';
+
+import './Post.css';
+import { ChatBubbleOutline, ThumbUp, NearMe, AccountCircle, ExpandMoreOutlined } from '@material-ui/icons';
+
+const Post = ({ profilePic, message, timestamp, imgName, username }) => {
+    return (
+        <div className="post">
+            <div className="post__top">
+                <Avatar src={profilePic} className="post__avatar" />
+                <div className="post__topInfo">
+                    <h3>{username}</h3>
+                    <p>{new Date(parseInt(timestamp)).toUTCString()}</p>
+                </div>
+            </div>
+            <div className="post__bottom">
+                <p>{message}</p>
+                {/* Image is coming Later */}
+            </div>
+            <div className="post__options">
+                <div className="post__option">
+                    <ThumbUp />
+                    <p>Like</p>
+                </div>
+                <div className="post__option">
+                    <ChatBubbleOutline />
+                    <p>Comment</p>
+                </div>
+                <div className="post__option">
+                    <NearMe />
+                    <p>Share</p>
+                </div>
+                <div className="post__option">
+                    <AccountCircle />
+                    <ExpandMoreOutlined />
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Post
